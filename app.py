8ba95acbc689f6b5c6fc5a06d22c575a1c57e790
Anya Keller <anyaisanya@gmail.com>
2016-09-26 14:06:08 -0400

    added util files

--- app.py
+++ app.py
@@ -1,54 +1,7 @@
+import util.testmod, util.occupations
 from flask import Flask, render_template
 app = Flask(__name__)
 
-#Random Occupation code
-import random
-# Opens and reads file into stringthing
-file=open("occupations.csv", "r")
-stringthing = file.read()
-file.close()
-
-# Splits the string into an array called splitString
-splitString = str.split(stringthing, "\r\n")
-
-dict = {}
-
-'''
-# Loops through array line by line
-for line in splitString:
-	if "Total" not in line:
-		if len(line)>0 and line[0]=='"':
-			line = line[1:]
-			dict[float(line[line.index('"')+2:])]=line[0:line.index('"')]
-		elif len(line)>0 and splitString.index(line)!=0:
-			#print line
-			dict[float(line[line.index(',')+1:])]=line[0:line.index(',')]
-'''
-
-for line in splitString:
-	if "Total" not in line and "Job" not in line:
-		if line.count(",") == 2: # if there are no parenthesis 
-			#print line
-			splitLine = line.split(",")
-			dict[float(splitLine[2])] = [splitLine[0],splitLine[1]]
-		elif line.count(",") > 2: #deal with extra commas
-			line = line[1:]
-			jobTitle = line[:line.index('"')]
-			line = line[line.index('"')+2:]
-			dict[float(line[line.index(",")+1:])] = [jobTitle, line[:line.index(",")]]
-print dict
-
-#because we have to make this a function...
-def getRandomOccupation():
-	#get a random number from 0 to 997 <-- although the toal is 99.8, we're working with integers and randint() is inclusive of zero
-	r = random.randint(0,997)
-	temp = 0 #keeps track of probability range
-	for key in dict:
-		temp += 10 * key
-		if r < temp:
-			return dict[key]
-		if temp == 859:
-			return dict[key]
 
 #Start of routes
 @app.route("/")
@@ -60,12 +13,13 @@ def index():
 @app.route("/occupations")
 
 def occupations():
-	return render_template("occupations.html", table = dict, occ = getRandomOccupation())
+	return util.occupations.render()
+	#return render_template("occupations.html", table = dict, occ = getRandomOccupation())
 
-@app.route("/morestuff")
+@app.route("/testmod")
 
-def morestuff():
-	return render_template("index.html")
+def testmodPage():
+	return util.testmod.test()
 
 if __name__ == "__main__":
 	#app.debug = True #
---
+++ util/occupations.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template
+#Random Occupation code
+import random
+# Opens and reads file into stringthing
+file=open("occupations.csv", "r")
+stringthing = file.read()
+file.close()
+
+# Splits the string into an array called splitString
+splitString = str.split(stringthing, "\r\n")
+
+dict = {}
+
+'''
+# Loops through array line by line
+for line in splitString:
+	if "Total" not in line:
+		if len(line)>0 and line[0]=='"':
+			line = line[1:]
+			dict[float(line[line.index('"')+2:])]=line[0:line.index('"')]
+		elif len(line)>0 and splitString.index(line)!=0:
+			#print line
+			dict[float(line[line.index(',')+1:])]=line[0:line.index(',')]
+'''
+
+for line in splitString:
+	if "Total" not in line and "Job" not in line:
+		if line.count(",") == 2: # if there are no parenthesis 
+			#print line
+			splitLine = line.split(",")
+			dict[float(splitLine[2])] = [splitLine[0],splitLine[1]]
+		elif line.count(",") > 2: #deal with extra commas
+			line = line[1:]
+			jobTitle = line[:line.index('"')]
+			line = line[line.index('"')+2:]
+			dict[float(line[line.index(",")+1:])] = [jobTitle, line[:line.index(",")]]
+#print dict
+
+#because we have to make this a function...
+def getRandomOccupation():
+	#get a random number from 0 to 997 <-- although the toal is 99.8, we're working with integers and randint() is inclusive of zero
+	r = random.randint(0,997)
+	temp = 0 #keeps track of probability range
+	for key in dict:
+		temp += 10 * key
+		if r < temp:
+			return dict[key]
+		if temp == 859:
+			return dict[key]
+
+def render():
+	return render_template("occupations.html", table = dict, occ = getRandomOccupation())
